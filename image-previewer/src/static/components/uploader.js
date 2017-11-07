5b075970227185b2ff6cc32abe08cafc4403d425
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-11-07 15:57:40 -0800

    switched to react-dropzone

--- image-previewer/src/static/components/uploader.js
+++ image-previewer/src/static/components/uploader.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
+import Dropzone from 'react-dropzone';
 
 class Uploader extends Component{
     render(){
         return(
             <div className="row">
-            <div className="col-10" >
-              <form className="form-group">
-                    <input type="file" className="form-control" placeholder="Browse an image" />
-              </form>
-            </div>
+              <div className="col-10">
+                <Dropzone className="uploader" multiple={false} accept="image/*">
+                    <p className="text-center">Drop an image or click to select a file to upload.</p>
+                </Dropzone>
+              </div>
+               
+
             <div className="col-2">
               <button type="submit" className="btn btn-primary btn-large">Upload</button>
             </div>
