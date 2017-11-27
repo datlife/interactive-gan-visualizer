03edb06a3179dc3498750a6bdd635d6129cae40d
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-11-26 21:39:26 -0800

    Simplified YOLOv2

--- detection_model/models/YOLOv2.py
+++ detection_model/models/YOLOv2.py
@@ -1,65 +1,92 @@
-"""
-YOLOv2 Meta-Architecture
-
-        images --> feature extractor --> feature map --> detector --> output feature map
-
-In this file, we define three different detector:
-   * Original YOLOv2 Detector
-
-"""
-import numpy as np
+"""YOLOv2 Model Definition"""
 import tensorflow as tf
 import keras.backend as K
 
-from keras.layers import Lambda
-from keras.models import Model
+from keras.layers import Conv2D
+from keras.layers import MaxPool2D
+from keras.layers import BatchNormalization
+from keras.layers import Lambda, concatenate
+from keras.layers.advanced_activations import LeakyReLU
+from keras.regularizers import l2
 
-from .detector import yolov2_detector
-from .custom_layers import PostProcessor
-from .utils import interpret_prediction
-
-FINE_GRAINED_LAYERS = {'yolov2': 'leaky_re_lu_13'}
-DETECTOR = {'yolov2': yolov2_detector}
+from .custom_layers import Preprocessor, PostProcessor, Reroute
 
 
 class YOLOv2(object):
-    def __init__(self,
-                 is_training,
-                 img_size,
-                 num_classes,
-                 anchors,
-                 feature_extractor,
-                 detector
-                 ):
-        """
-        YOLOv2 Meta-Architecture
+    def __init__(self, anchors, num_classes, preprocess_func):
+        self.anchors         = anchors
+        self.num_classes     = num_classes
+        self.preprocess_func = preprocess_func
 
-        images --> feature extractor --> feature map --> detector --> output feature map
+    def predict(self, resized_inputs):
 
-        :param num_classes:
-        :param img_size
-        :param anchors:
-        :param is_training:
-        :param feature_extractor:
-        :param detector:
-        """
-        self._is_training      = is_training
+        anchors, num_classes = self.anchors, self.num_classes
+        preprocess_func = self.preprocess_func
 
-        self.num_classes       = num_classes
-        self.anchors           = anchors
-        self.img_size          = img_size
+        # ##################
+        # Feature Extractor
+        # ##################
+        x = Preprocessor(preprocess_func, name='preprocessor')(resized_inputs)
 
-        self.feature_extractor = feature_extractor
-        self.fine_grained_layers = FINE_GRAINED_LAYERS[feature_extractor.name]
-        self.detector = DETECTOR[detector](feature_extractor, num_classes, len(anchors), self.fine_grained_layers)
+        x = conv_block(x, 32, (3, 3))  # << --- Start feature extraction
+        x = MaxPool2D(strides=2)(x)
 
-        # YOLOv2 Model
-        self.model = Model(inputs=feature_extractor.model.input,
-                           outputs=self.detector)
+        x = conv_block(x, 64, (3, 3))
+        x = MaxPool2D(strides=2)(x)
+
+        x = conv_block(x, 128, (3, 3))
+        x = conv_block(x, 64, (1, 1))
+        x = conv_block(x, 128, (3, 3))
+        x = MaxPool2D(strides=2)(x)
+
+        x = conv_block(x, 256, (3, 3))
+        x = conv_block(x, 128, (1, 1))
+        x = conv_block(x, 256, (3, 3))
+        x = MaxPool2D(strides=2)(x)
+
+        x = conv_block(x, 512, (3, 3))
+        x = conv_block(x, 256, (1, 1))
+        x = conv_block(x, 512, (3, 3))
+        x = conv_block(x, 256, (1, 1))
+        x = conv_block(x, 512, (3, 3))
+        fine_grained_layer = x
+        x = MaxPool2D(strides=2)(x)
+
+        x = conv_block(x, 1024, (3, 3))
+        x = conv_block(x, 512, (1, 1))
+        x = conv_block(x, 1024, (3, 3))
+        x = conv_block(x, 512, (1, 1))
+        feature_map = conv_block(x, 1024, (3, 3))  # ---> feature extraction ends here
+
+        # ################
+        # Object Detector
+        # ################
+        x = conv_block(feature_map, 1024, (3, 3))
+        x = conv_block(x, 1024, (3, 3))
+        x2 = x
+
+        # Reroute
+        x = conv_block(fine_grained_layer, 64, (1, 1))
+        x = Reroute(block_size=2, name='space_to_depth_x2')(x)
+
+        x = concatenate([x, x2])
+        x = conv_block(x, 1024, (3, 3))
+
+        prediction = Conv2D(len(anchors) * (num_classes + 5), (1, 1), name='yolov2')(x)
+
+        return prediction
 
     def post_process(self, prediction, iou_threshold=0.5, score_threshold=0.6):
         """
-        Input: out feature map from network
+        Preform non-max suppression to calculate outputs:
+            * Bounding Boxes
+            * Classes
+            * Scores (Probabilities)
+
+        Using during evaluation/interference
+
+        Args:
+            out feature map from network
 
         Output:
            Bounding Boxes - Classes - Probabilities
@@ -69,15 +96,13 @@ class YOLOv2(object):
                                 name="non_max_suppression")(prediction)
         boxes   = Lambda(lambda x: x[..., :4], name="boxes")(outputs)
         scores  = Lambda(lambda x: x[..., 4],  name="scores")(outputs)
-        classes = Lambda(lambda x: K.cast(x[..., 5], tf.int8),  name="classes")(outputs)
+        classes = Lambda(lambda x: K.cast(x[..., 5], tf.float32),  name="classes")(outputs)
 
         return boxes,  classes, scores
 
     def loss_func(self, y_true, y_pred):
         """
         YOLOv2 Loss Function Implementation
-
-        Input: out feature map from network
 
         Output:
            A scalar - loss value for back propagation
@@ -167,3 +192,59 @@ class YOLOv2(object):
         loss = 0.5 * (loc_loss + obj_conf_loss + category_loss)
 
         return loss
+
+# ##############
+# HELPER METHODS
+# ##############
+
+
+def conv_block(x, filters, kernel_size, name=None):
+    """
+    Standard YOLOv2 Convolutional Block as suggested in YOLO9000 paper
+    """
+    x = Conv2D(filters     = filters,
+               kernel_size = kernel_size,
+               padding     = 'same',
+               use_bias    = False, name=name)(x)
+    x = BatchNormalization(name=name if name is None else 'batch_norm_%s' % name)(x)
+    x = LeakyReLU(alpha=0.1, name=name if name is None else 'leaky_relu_%s' % name)(x)
+    return x
+
+
+def interpret_prediction(prediction, anchors, num_classes):
+    N_CLASSES = num_classes
+    N_ANCHORS = len(anchors)
+    ANCHORS = anchors
+
+    pred_shape = tf.shape(prediction)
+    GRID_H, GRID_W = pred_shape[1], pred_shape[2]
+
+    prediction = K.reshape(prediction, [-1, pred_shape[1], pred_shape[2], N_ANCHORS, N_CLASSES + 5])
+
+    # Create off set map
+    cx = tf.cast((K.arange(0, stop=GRID_W)), dtype=tf.float32)
+    cx = K.tile(cx, [GRID_H])
+    cx = K.reshape(cx, [-1, GRID_H, GRID_W, 1])
+
+    cy = K.cast((K.arange(0, stop=GRID_H)), dtype=tf.float32)
+    cy = K.reshape(cy, [-1, 1])
+    cy = K.tile(cy, [1, GRID_W])
+    cy = K.reshape(cy, [-1])
+    cy = K.reshape(cy, [-1, GRID_H, GRID_W, 1])
+
+    c_xy = tf.stack([cx, cy], -1)
+    c_xy = tf.to_float(c_xy)
+
+    anchors_tensor = tf.to_float(K.reshape(ANCHORS, [1, 1, 1, N_ANCHORS, 2]))
+    netout_size = tf.to_float(K.reshape([GRID_W, GRID_H], [1, 1, 1, 1, 2]))
+
+    box_xy = K.sigmoid(prediction[..., :2])
+    box_wh = K.exp(prediction[..., 2:4])
+    box_confidence = K.sigmoid(prediction[..., 4:5])
+    box_class_probs = K.softmax(prediction[..., 5:])
+
+    # Shift center points to its grid cell accordingly (Ref: YOLO-9000 loss function)
+    box_xy = (box_xy + c_xy) / netout_size
+    box_wh = (box_wh * anchors_tensor) / netout_size
+
+    return box_xy, box_wh, box_confidence, box_class_probs
