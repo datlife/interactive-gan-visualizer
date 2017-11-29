6f4a9ba2f6e428691ded9e31c4d1612b58a4fd80
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-11-29 02:37:34 -0800

    updated webcam

--- detection/webcam_demo.py
+++ detection/webcam_demo.py
@@ -17,42 +17,56 @@ tf.app.flags.DEFINE_string('model', 'ssd', 'tf serving model (yolov2, ssd, faste
 FLAGS = tf.app.flags.FLAGS
 
 model = FLAGS.model
+print(model)
 detector = ObjectDetectionServer('localhost:9000', model, get_labels(model))
 
+import time
 
 def main(_):
     video_capture = WebcamVideoStream(0).start()
-    fps = FPS().start()
 
-    input_q  = Queue(maxsize=5)
-    output_q = Queue(maxsize=2)
+    input_q  = Queue(maxsize=3)
+    output_q = Queue(maxsize=1)
     pool     = Pool(1, worker, (input_q, output_q))
 
     boxes   = []
     classes = []
     scores  = []
+    
+    camera_fps    = 0
+    detection_fps = 0
+
+    num_frames       = 0
+    detection_frames = 0
+
+    start = time.time()
     while True:
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         frame = video_capture.read()
 
 	if input_q.full():
-	  input_q.get()
+	    input_q.get()
+
         input_q.put(frame)
 
         if not output_q.empty():
            boxes, classes, scores = output_q.get()
+	   detection_frames+=1
+           detection_fps = detection_frames / (time.time() - start)
 
-        frame = draw(frame, boxes, classes, scores)
-        print("Running...", input_q.qsize())
+	num_frames +=1
+        camera_fps = num_frames / (time.time() - start)
+
+        frame = draw(frame, boxes, classes, scores, camera_fps, detection_fps)
         cv2.imshow('Video', frame)
-        fps.update()
 
-    fps.stop()
+    elapsed = (time.time() - start)
+    print('[INFO] elapsed time (total): {:.2f}'.format(elapsed))
+    print('[INFO] approx. FPS: {:.2f}'.format(num_frames / elapsed))
+    print('[INFO] approx. detetcion FPS: {:.2f}'.format(detection_frames/ elapsed))
     video_capture.stop()
     cv2.destroyAllWindows()
-    print('[INFO] elapsed time (total): {:.2f}'.format(fps.elapsed()))
-    print('[INFO] approx. FPS: {:.2f}'.format(fps.fps()))
 
 
 def worker(input_q, output_q):
@@ -69,12 +83,12 @@ def detect_objects_in(frame):
 
     predict = time.time()
     data = detector.predict(frame)
-    print("Prediction in {}".format(time.time()-predict))
+#    print("Prediction in {}".format(time.time()-predict))
     boxes, classes, scores = filter_out(threshold=0.5, data=data)
     return boxes, classes, scores
 
 
-def draw(img, bboxes, classes, scores):
+def draw(img, bboxes, classes, scores, fps, detection_fps):
     """
     Drawing Bounding Box on Image
     :param img:
@@ -108,6 +122,8 @@ def draw(img, bboxes, classes, scores):
 
         draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=tuple(color))
         draw.text(tuple(text_origin), label, fill=(0, 0, 0), label_size=2, font=font)
+        draw.text((10, height - 20), "Camera FPS: {:.2f} fps".format(fps), fill=(0, 255, 0), label_size=3, font=font)
+        draw.text((width - 200, height - 20), "Detection FPS: {:.2f} fps".format(detection_fps), fill=(0, 255, 0), label_size=3, font=font)
     del draw
     return np.array(image)
 
