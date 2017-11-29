460f094d160648fceb98e8cbf25364fefe6bfb9e
Dat Nguyen <tdat.nguyen93@gmail.com>
2017-11-29 02:37:49 -0800

    Updated webcamdemo

--- detection/src/utils/webcam.py
+++ detection/src/utils/webcam.py
@@ -42,8 +42,8 @@ class FPS(object):
     def __init__(self):
         # store the start time, end time, and total number of frames
         # that were examined between the start and end intervals
-        self._start = None
-        self._end = None
+        self._start = datetime.datetime.now()
+        self._end = datetime.datetime.now()
         self._numFrames = 0
 
     def start(self):
@@ -62,9 +62,13 @@ class FPS(object):
 
     def elapsed(self):
         # return the total number of seconds between the start and
-        # end interval
-        return (self._end - self._start).total_seconds()
+        if self._start is None: return -1.0
+        return (datetime.datetime.now() - self._start).total_seconds()
 
     def fps(self):
         # compute the (approximate) frames per second
+	if self._numFrames < 5:
+            return -1.0
         return self._numFrames / self.elapsed()
+
+
